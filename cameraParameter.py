ca2d7da61e97130a2b29c1c15e0a662fe4d97bed
savent <savent_gate@outlook.com>
2021-05-17 19:48:33 +0800

    feat: add camera calibration

--- cameraParameter.py
+++ cameraParameter.py
@@ -1,6 +1,8 @@
 import numpy as np
 
+
 class CameraParameter:
-    def __init__(self, inner=np.eye(3), outer=np.eye(4)):
+    def __init__(self, inner=np.eye(3), outer=np.eye(4), calibration=None):
         self.inner = inner
         self.outer = outer
+        self.calibration = calibration
--- frameFactory.py
+++ frameFactory.py
@@ -4,6 +4,7 @@ import numpy as np
 import cv2 as cv
 import copy
 
+
 class FrameFactory:
     def get_frame():
         return Frame()
@@ -11,28 +12,36 @@ class FrameFactory:
     def is_eof():
         return True
 
+
 class SimulateFrameFactory(FrameFactory):
     def __init__(self, dir='datasets'):
         '''
         创建实例
         '''
-        fileName =  dir + '/pos.txt'
+        fileName = dir + '/pos.txt'
         imageDirectory = dir + '/images/'
 
-        DOFs = np.genfromtxt(fileName, delimiter=",", usecols=range(1, 7), dtype=float)
-        imgList = np.genfromtxt(fileName, delimiter=",", usecols=[0], dtype=str)
-        imgList = [ "{}/{}".format(imageDirectory, iter) for iter in imgList]
+        DOFs = np.genfromtxt(fileName, delimiter=",",
+                             usecols=range(1, 7), dtype=float)
+        imgList = np.genfromtxt(fileName, delimiter=",",
+                                usecols=[0], dtype=str)
+        imgList = ["{}/{}".format(imageDirectory, iter) for iter in imgList]
 
-        default_inner_matrix = [[1399, 0, 912.84], [0, 1398, 560.97, 0, 0, 1]]
-        camParam = CameraParameter(inner=default_inner_matrix)
+        default_inner_matrix = np.float32([[1399.008608021883, 0, 912.8436694988062], [
+                                          0, 1398.067750138546, 560.9715646208826], [0, 0, 1]])
+        default_camera_calibration = np.float32([-0.1928987635480564, 1.185777385330833, -
+                                                 0.008740735312992649, -0.00720632424414056, -4.413429081461387])
+        default_camera_calibration = None # 畸变参数不准确，忽略畸变矫正
+        camParam = CameraParameter(
+            inner=default_inner_matrix, calibration=default_camera_calibration)
 
         self.m_method = "simulate"
-        self.m_DOFs = DOFs;
+        self.m_DOFs = DOFs
         self.m_img_list = imgList
         self.m_index = 0
         self.m_cam_param = camParam
 
-    def get_frame(self): 
+    def get_frame(self):
         if self.is_eof():
             return None
 
@@ -42,12 +51,25 @@ class SimulateFrameFactory(FrameFactory):
             img_path = self.m_img_list[index]
             dof = self.m_DOFs[index]
             img = cv.imread(img_path)
+            img = self.__calibration(img)
             self.m_index = self.m_index + 1
             cam_param = copy.copy(self.m_cam_param)
         return Frame(img, cam_param)
-        
+
     def is_eof(self):
         '''
         判断是否需要结束
         '''
         return self.m_index >= len(self.m_img_list)
+
+    def __calibration(self, image):
+        '''
+        修正图像畸变
+        '''
+        k = self.m_cam_param.inner
+        d = self.m_cam_param.calibration
+
+        h, w = image.shape[:2]
+        mapx, mapy = cv.initUndistortRectifyMap(
+            k, d, None, None, (w, h), cv.CV_32FC1)
+        return cv.remap(image, mapx, mapy, cv.INTER_LINEAR)
