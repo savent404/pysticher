1f9b810ba3cc1fc96d61bddbcb3f19bcb6d18802
savent <savent_gate@outlook.com>
2021-05-19 19:00:30 +0800

    feat: remove main.py
    using sticher.py as default entry

--- main.py
+++
@@ -1,9 +0,0 @@
-from sticher import Sticher
-from frameFactory import SimulateFrameFactory
-
-if __name__ == '__main__':
-    ff = SimulateFrameFactory()
-
-    sticher = Sticher([ff])
-    sticher.run()
-    sticher.download_map('./result.jpg')
